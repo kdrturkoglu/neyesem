7445880b9926bbe11cdaea36cd535403997d4457
kdrturkoglu <kdrkiret@gmail.com>
2021-01-20 23:24:54 +0300

    radio button eklendi

--- lib/Anasayfa.dart
+++ lib/Anasayfa.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:neyesem/sample.dart';
 
-class Anasayfa extends StatelessWidget {
+class Anasayfa extends StatefulWidget {
+  Anasayfa(): super();
+  @override
+  State<StatefulWidget> createState() {
+    return SenecekWidgetState();
+  }
+}
+
+class SenecekWidgetState extends State<Anasayfa>{
+
+  int selectedRadio;
+  String text;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedRadio = 0;
+    text = "";
+  }
+
+
+  setSelectedRadio(int val,String b){
+    setState(() {
+      selectedRadio = val;
+      text = b;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
             body: Container(
@@ -19,15 +46,38 @@ class Anasayfa extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     verticalDirection: VerticalDirection.up,
                     children: <Widget>[ 
-                    RaisedButton.icon(
-                      onPressed: () => Navigator.push( context,MaterialPageRoute(builder: (context) => Sample())),
-                      shape: RoundedRectangleBorder( borderRadius:BorderRadius.all(Radius.circular(10.0))),
-                      label: Text('Ne Yesem', style: TextStyle(color: Colors.white), ),
-                      icon: Icon(Icons.restaurant,color: Colors.white,),
-                      textColor: Colors.white,
-                      splashColor: Colors.grey[900],
-                      color: Colors.lime[900],
-                    )
+                       Text(text, style: TextStyle(color: Colors.white), ),
+                      RaisedButton.icon(
+                          onPressed: () => Navigator.push( context,MaterialPageRoute(builder: (context) => Sample())),
+                          shape: RoundedRectangleBorder( borderRadius:BorderRadius.all(Radius.circular(10.0))),
+                          label: Text('Ne Yesem', style: TextStyle(color: Colors.white), ),
+                          icon: Icon(Icons.restaurant,color: Colors.white,),
+                          textColor: Colors.white,
+                          splashColor: Colors.grey[900],
+                          color: Colors.lime[900],
+                          ),
+                      ButtonBar(
+                        alignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                        Radio(value: 1,
+                          groupValue: selectedRadio, 
+                          activeColor: Colors.green,
+                          onChanged: (val){
+                            print("Radio $val");
+                            setSelectedRadio(val,"Tıklandı 1");
+                          }
+                         ),
+                         Radio(value: 2,
+                          groupValue: selectedRadio, 
+                          activeColor: Colors.blue,
+                          onChanged: (val){
+                            print("Radio $val");
+                            setSelectedRadio(val,"Tıklandı 2");
+                          }
+                         )
+                        ],
+                      ),
+                   
                   ],
                ), 
             ]
